c061ed7d9093e8e444b78573f9d756de223035cf
bchauvet <bchauvet@NXQLX140.nx-domain.lan>
2020-11-09 16:35:58 +0100

    recursivite

---
+++ Recursivite/src/Factorielle.java
@@ -0,0 +1,25 @@
+
+public class Factorielle {
+
+	public static void main(String[] args) {
+	
+		//fact(10) 10!
+		// 10*9*8*7*6*5*4*3*2*1
+		// 0!  =   1
+		
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static int factorielle(int n)
+	{
+		return 0;
+	}
+	
+	public static int factorielleR(int n)
+	{
+		return 0;
+	}
+	
+
+}
